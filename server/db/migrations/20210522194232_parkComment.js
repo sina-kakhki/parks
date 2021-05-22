5396b186193223d0e64b0ee12a54c0a52b3bffe9
sina <ksina.72@gmail.com>
2021-05-22 20:06:33 +1200

    reran migrations

--- server/db/migrations/20210522194232_parkComment.js
+++ server/db/migrations/20210522194232_parkComment.js
@@ -1,8 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('parkComment', table => {
+    table.increments('id')
+    table.int('park_id')
+    table.string('user_name')
+    table.string('comment')
+    table.int('rating')
+  })
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('parkComment')
+}
