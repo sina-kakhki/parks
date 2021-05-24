a94ba48190024488c21059862db2871c05eaf278
sina <ksina.72@gmail.com>
2021-05-24 12:12:27 +1200

    modified migration to deploy to heroku

--- server/db/migrations/20210522194232_parkComment.js
+++ server/db/migrations/20210522194232_parkComment.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('parkComment', table => {
     table.increments('id')
-    table.int('park_id')
+    table.integer('park_id')
     table.string('user_name')
     table.string('comment')
     table.int('rating')
