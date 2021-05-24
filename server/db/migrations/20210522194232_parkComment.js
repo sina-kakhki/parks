67b30f50c2541d909288bf5d90aaa8c778f4ac1a
sina <ksina.72@gmail.com>
2021-05-24 12:16:00 +1200

    another modification for migration hopefully this works

--- server/db/migrations/20210522194232_parkComment.js
+++ server/db/migrations/20210522194232_parkComment.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('parkComment', table => {
     table.increments('id')
-    table.integer('park_id')
+    table.integer('park_id').references('id').inTable('parks')
     table.string('user_name')
     table.string('comment')
     table.int('rating')
